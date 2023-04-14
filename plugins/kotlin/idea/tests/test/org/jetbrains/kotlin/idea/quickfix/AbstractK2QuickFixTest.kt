e507687efcaa0d54cc16db23d9eb83388dd3a4ba
Justin Paupore <jpaupore@google.com>
2023-04-13 18:09:57 -0700

    Rearrange inspection and quickfix tests.
    - Move AbstractInspectionTest to the inspections package, and
      AbstractQuickFixTest to the quickfix package.
    - Move AbstractK2QuickFixTest to the inspections package, so that
      parcelize can use this for K2 parcelize tests.
    
    ^KTIJ-25177

--- plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/quickfix/AbstractK2QuickFixTest.kt
+++ plugins/kotlin/idea/tests/test/org/jetbrains/kotlin/idea/quickfix/AbstractK2QuickFixTest.kt
@@ -1,5 +1,5 @@
 // Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
-package org.jetbrains.kotlin.idea.k2.inspections.tests
+package org.jetbrains.kotlin.idea.quickfix
 
 import org.jetbrains.kotlin.idea.quickfix.AbstractQuickFixTest
 import org.jetbrains.kotlin.test.utils.IgnoreTests
